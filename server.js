1b86b9d30a18c1a905eb5c345d92712abcc66f18
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-02-25 12:56:44 +0530

    adding tags

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+app.get('/beneficiary', (req, res) => {
+  res.send('Beneficiary app\n');
+});
+
+app.get('/frontend', (req, res) => {
+  res.send('Frontend app\n');
 });
 
 app.listen(PORT, HOST);
